71515822de020b311ba61e1447398b914866be8b
Kendall77777 <kjcartonia@scswarriors.com>
2025-03-04 09:39:02 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,24 @@
+namespace SpriteKind {
+    export const Food2 = SpriteKind.create()
+    export const Food3 = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food3, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    sprites.destroy(Chest3)
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     info.changeScoreBy(1)
     sprites.destroy(Chest)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food2, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    sprites.destroy(Chest2)
+})
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.floorDark3, function (sprite, location) {
     game.gameOver(true)
 })
+let Chest3: Sprite = null
+let Chest2: Sprite = null
 let Chest: Sprite = null
 tiles.setCurrentTilemap(tilemap`level3`)
 let mySprite = sprites.create(img`
@@ -55,7 +69,45 @@ Chest = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Food)
+Chest2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f f f . . . . 
+    . . . . f e e e e e e e f . . . 
+    . . . f e e 5 5 5 5 5 e e f . . 
+    . . . f f f 5 f f f 5 f f f . . 
+    . . . f e e 5 5 5 5 5 e e f . . 
+    . . . f e e e e e e e e e f . . 
+    . . . f e e e e e e e e e f . . 
+    . . . f e e e e e e e e e f . . 
+    . . . f f f f f f f f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Food2)
+Chest3 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f f f . . . . 
+    . . . . f e e e e e e e f . . . 
+    . . . f e e 5 5 5 5 5 e e f . . 
+    . . . f f f 5 f f f 5 f f f . . 
+    . . . f e e 5 5 5 5 5 e e f . . 
+    . . . f e e e e e e e e e f . . 
+    . . . f e e e e e e e e e f . . 
+    . . . f e e e e e e e e e f . . 
+    . . . f f f f f f f f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Food3)
 Chest.setPosition(randint(0, 120), randint(0, 120))
+Chest2.setPosition(randint(0, 120), randint(0, 120))
+Chest3.setPosition(randint(0, 120), randint(0, 120))
 let statusbar = statusbars.create(20, 4, StatusBarKind.Health)
 statusbar.attachToSprite(mySprite)
 statusbar.value = 100
